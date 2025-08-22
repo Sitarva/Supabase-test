293ec28e710c9039d5e3557eb217c7b1daecdb86
Sitarva <168885163+Sitarva@users.noreply.github.com>
2025-08-22 11:44:03 +1000

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,4 @@
 const SUPABASE_URL = "https://ucqoiltqcblrwkltglos.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjcW9pbHRxY2JscndrbHRnbG9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU3Nzc1NTUsImV4cCI6MjA3MTM1MzU1NX0.d9nusguupaLupLRa1Yn7pBAgzJ9d2eU4Sx-SrgRAFcI";
 const IMAGES_BUCKET = "flashcards-images";
-const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseClient = window.Supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
